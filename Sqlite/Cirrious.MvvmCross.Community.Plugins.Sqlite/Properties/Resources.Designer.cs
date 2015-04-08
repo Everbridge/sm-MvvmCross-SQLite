5673a62ba4e17654423975cd8814502fced3faa0
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2015-04-07 23:02:33 -0400

    PCL 4.5 get assembly fixed

--- Sqlite/Cirrious.MvvmCross.Community.Plugins.Sqlite/Properties/Resources.Designer.cs
+++ Sqlite/Cirrious.MvvmCross.Community.Plugins.Sqlite/Properties/Resources.Designer.cs
@@ -41,7 +41,7 @@ namespace Cirrious.MvvmCross.Community.Plugins.Sqlite.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Cirrious.MvvmCross.Community.Plugins.Sqlite.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Cirrious.MvvmCross.Community.Plugins.Sqlite.Properties.Resources", typeof(Resources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
